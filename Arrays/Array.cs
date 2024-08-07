5208a48b4efd0e4df892476aec503f05e0f701cb
farid <farid@LAPTOP-SPTVSQVM>
2024-08-08 00:07:29 +0530

    Add project files.

---
+++ Arrays/Array.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Arrays_Concept
+{
+    public class Array
+    {
+        public void GetValue(int index)
+        {
+            int[] array = new int[index];
+            Console.WriteLine("enter the values in your array:");
+            for (int i = 0;i<index;i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("your array is");
+            for(int i = 0;i<index;i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+
+    
+}
---
+++ Arrays/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel;
+using System.Threading.Channels;
+
+namespace Arrays_Concept
+{
+    public class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Array array = new Array();  
+            Console.WriteLine("enter the no pf elements in array");
+            int Index = int.Parse(Console.ReadLine());
+            array.GetValue(Index);
+        }
+    }
+
+    
+}
